66a4812bdcd1a82833aa42fbf846f6098a7d927d
J Miles <j.milesy.nz@gmail.com>
2017-11-13 14:49:42 +1300

    Add params.rb for holding common parameters

---
+++ lib/printed/params.rb
@@ -0,0 +1,8 @@
+$mattress_w = 92 + 8
+$mattress_l = 188 + 12
+$mattress_h = 20
+
+$frame_board_w = 10
+$frame_board_h = 20
+$frame_board_l = ($mattress_w + $mattress_l + $frame_board_w*4)/3.0
+$frame_board_c = 3 # number of boards to stack
